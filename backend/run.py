35278d8a489607e9dc2e32107fcacefc5b2b8870
Jakub Gongol <jakub.gongol@outlook.com>
2018-10-03 17:42:10 +0200

    First ATOM commit

---
+++ backend/run.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"about":"Hello world"}
+
+class JsonExample(Resource):
+    def post(self):
+        req_data = request.get_json()
+        return jsonify(req_data)
+
+api.add_resource(HelloWorld,"/")
+api.add_resource(JsonExample,"/json-example")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
